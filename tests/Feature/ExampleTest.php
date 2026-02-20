b216369c00eaa3e435261723059199e357f4089a
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-20 10:50:59 +0700

    test(withdraw): add basic feature test and testing environment setup

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Juzaweb\Modules\Withdraw\Tests\Feature;
+
+use Juzaweb\Modules\Withdraw\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
